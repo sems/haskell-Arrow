6a3d064440825c7864e8284a21c3182b31308570
Sem <me@sems.dev>
2021-12-14 20:35:39 +0100

    Pre ex 7 indenting fix

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -25,22 +25,18 @@ type Space     =  Map Pos Contents
 -- | Parses a space file that can be found in the examples folder.
 parseSpace :: Parser Char Space
 parseSpace = do
-    (mr, mc) <- parenthesised ((,) <$> natural <* symbol ',' <*> natural)
-                <* spaces
+    (mr, mc) <- parenthesised ((,) <$> natural <* symbol ',' <*> natural) <* spaces
     -- read |mr + 1| rows of |mc + 1| characters
     css      <- replicateM (mr + 1) (replicateM (mc + 1) contents)
     -- convert from a list of lists to a finite map representation
     return $ L.fromList $ concat $
             zipWith (\r cs ->
-              zipWith (\c d -> ((r, c), d)) [0..] cs) [0..] css
+            zipWith (\c d -> ((r, c), d)) [0..] cs) [0..] css
   where
     spaces :: Parser Char String
     spaces = greedy (satisfy isSpace)
-
     contents :: Parser Char Contents
-    contents = choice (Prelude.map (\(f,c) -> f <$ symbol c) contentsTable)
-      <* spaces
-
+    contents = choice (Prelude.map (\(f,c) -> f <$ symbol c) contentsTable) <* spaces
 
 -- | Conversion table
 contentsTable :: [ (Contents, Char)]
