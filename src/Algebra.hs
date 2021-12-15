e651b0b24cf8ba5870159f0c49870ac791195847
Amberbm <56920715+Amberbm@users.noreply.github.com>
2021-12-15 13:34:14 +0100

    comments -ex6

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -28,39 +28,43 @@ type Algebra program rule cmds cmd dir alts alt contents = (
     contents -- Underscore Contents
   )
 
+-- up untill the end the bool is only that will be given along the elements is an idication on whether for all present caseof's there is an possebility for patternmatch failure
+-- the list of strings that will be passed along will end in the collection of all rules that are called upon withing a command so they can be checked on said rules existance at the end
+-- any element that does not contain a caseof / Ident  will be prepresented as True / [] respectively
+-- when a bool withing a element is False the list of string may be ignored because the program wil be considered False regadless
 algCheck :: Algebra Bool (Bool,String,[String]) (Bool,[String]) (Bool,[String]) Dir (Bool,[String]) (Bool,Contents,[String]) Contents
 algCheck = (fprogram ,frule, fcmds, (True,[]),(True,[]), (True,[]), (True, [] ), fturn, fcase, fident, Lef, Righ, Fron, falts, falt, Empty, Lambda, Debris, Asteroid, Boundary, Underscore )    
 
 fprogram :: [(Bool,String,[String])] -> Bool
-fprogram p = checkprog p [] []
-  where checkprog ((False,_,_):_) _ _ = False
-        checkprog [] rs ss | notElem "start" rs = False
+fprogram p = checkprog p [] [] --  performs each check on the program
+  where checkprog ((False,_,_):_) _ _ = False --checks on False cases
+        checkprog [] rs ss | notElem "start" rs = False -- checks whether a "start" rule is among the program
                            | otherwise = checkStrings ss
-          where checkStrings [] = True
-                checkStrings (x:xs) | elem x rs = checkStrings xs 
+          where checkStrings [] = True --
+                checkStrings (x:xs) | elem x rs = checkStrings xs --checks for each Identcmd whether it's name actually exists among the rules
                                     | otherwise = False
-        checkprog ((_,rule,ss):xs) rs ts | elem rule rs = False
+        checkprog ((_,rule,ss):xs) rs ts | elem rule rs = False --checks on whether each rule was added only once
                                          | otherwise = checkprog xs (rule:rs) (ss++ts)
 
-frule :: String ->  (Bool,[String])  -> (Bool,String,[String])
+frule :: String ->  (Bool,[String])  -> (Bool,String,[String]) -- passes along the bool and list of string togther with it's own name 
 frule s (b,ss) = (b,s,ss)
 
-fcmds :: [(Bool,[String])] -> (Bool,[String])
+fcmds :: [(Bool,[String])] -> (Bool,[String]) -- combine the list of string among the lists of cmds while checking on the correctness of possible containing caseof's
 fcmds = checkCmds []
   where checkCmds _ ((False,_):_) = (False,[]) 
         checkCmds ss [] = (True,ss)
         checkCmds ts ((_,ss):xs) = checkCmds (ts++ss) xs
 
-fturn :: Dir -> (Bool,[String])
+fturn :: Dir -> (Bool,[String]) -- dir to be ignored , there is no caseof within this element nor a identcmd so returns true with empty string
 fturn d = (True,[])
 
-fcase :: Dir ->(Bool,[String]) -> (Bool,[String])
+fcase :: Dir ->(Bool,[String]) -> (Bool,[String]) -- dir is ignored, case got already checked alt alts stage
 fcase _ x = x 
 
-fident :: String -> (Bool,[String])
+fident :: String -> (Bool,[String]) -- puts the cmd name is a list so it can be passed along for testing on correctness
 fident s = (True,[s])
 
-falts :: [(Bool,Contents,[String])] -> (Bool,[String])
+falts :: [(Bool,Contents,[String])] -> (Bool,[String]) -- checks whether all contents(or an underscore) is incuded among the alts meaning the patternmatching within the case is non-exhaustive 
 falts alts =  checkAlts alts [] []
   where checkAlts :: [(Bool,Contents,[String])] -> [Contents] -> [String] -> (Bool,[String])
         checkAlts [] cs ss = (checkContents,ss)
@@ -68,7 +72,7 @@ falts alts =  checkAlts alts [] []
         checkAlts ((False,_, _):_) _ _= (False,[])
         checkAlts ((_,c,ss):xs) cs ts = checkAlts xs (c:cs) (ss++ts)
 
-falt :: Contents ->  (Bool,[String])-> (Bool,Contents,[String])
+falt :: Contents ->  (Bool,[String])-> (Bool,Contents,[String]) --passed along its content toghether with bool/[string] combination from the cmd is containded
 falt c (False,_) = (False,c,[])
 falt c (b,ss) = (b,c,ss)
 
